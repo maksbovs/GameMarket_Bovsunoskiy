8259fdd22563c914fd7b59e0040b1158f5b4a0ea
maksbovs <158279987+maksbovs@users.noreply.github.com>
2024-05-08 17:13:34 +0300

    .

--- market.js
+++ market.js
@@ -1,22 +1,24 @@
+//Перевірка підключеного файлу скриптів
+console.log('Перевірка підключеного файлу скриптів market.js');
+// Отримання елементу з ідентифікатором items 
 let itemsDiv = document.getElementById("items");
-console.log(itemsDiv);
-if(itemsDiv){
-    console.log(itemsDiv);
-    //console.log('Поле classList: ', itemsDiv.classList)
-    //console.log('Поле id: ', itemsDiv.id)
-    //console.log('Поле clientWidth: ', itemsDiv.classWidth)
-    //console.log('Поле innerHTML: ', itemsDiv.innerHTML)
 
-    //itemsDiv.innerText = 'повартсмщдшр'
-    //itemsDiv.innerHTML = '<h1>acoustic</h1>'
-    //itemsDiv.innerHTML = '<div class="item></div>"'
-    //itemsDiv.innerHTML = '<div class="item></div>"'
-    //itemsDiv.innerHTML = '<div class="item></div>"'
-    //itemsDiv.innerHTML = '<div class="item></div>"'
-    // for(let i=0;i<100;i++){
-    //    itemsDiv.innerHTML += '<div class="item></div>"'
-    // }
+//Перевірка існування знайденого блоку
+if (itemsDiv) {
+    //Вивід знайденого елементу 
+    console.log(itemsDiv)
+    for (let i = 0; i < 100; i++) {
+        itemsDiv.innerHTML += '<div class = "item"></div>'
+    }
+} else {
+    console.log('не знайдено')
 }
-else{
-    console.log('Блок товарів не знайдено')
+
+//viznachennya masivu
+let itemsArray = ['', '', '', '']
+//vivedennya masivu v consol
+console.log(itemsArray)
+//vivedennya elementiv masivu v consol
+for (let i = 0; i < itemsArray.length; i++) {
+    console.log(itemsArray[i])
 }
